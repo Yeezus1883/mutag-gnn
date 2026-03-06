8e0b493ba769d7879e58874009c9d1365bb00781
Yeezus1883 <103069496+Yeezus1883@users.noreply.github.com>
2026-03-06 12:59:59 +0530

    Edited style path

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from src.utils.graph_viz import draw_molecule_graph
 from src.utils.smiles_to_graph import smiles_to_graph
 import streamlit as st
 
-with open(".streamlit/style.css") as f:
+with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 DEVICE = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
